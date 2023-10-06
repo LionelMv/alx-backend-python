93fc8e2c4b6e62d085422c8f8a8ba5c964632c76
LionelMv <mvangicode@gmail.com>
2023-10-06 08:49:45 +0300

    fix: Add List annotation instead of list
    ALX is using Python version that is below 3.9 to check the code.

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 Takes a list of floats and returns their sum.
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Takes a list of floats, returns their sum.
     Arg: input_list (float)
