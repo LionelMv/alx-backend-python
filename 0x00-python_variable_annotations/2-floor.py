c0f0661a729deca271775c75f0e6722ef62467fe
LionelMv <mvangicode@gmail.com>
2023-10-06 08:14:37 +0300

    doc: Add docstring for the function.

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,4 +5,9 @@ This module takes a float and converts it into a integer.
 
 
 def floor(n: float) -> int:
+    """
+    Takes a float and converts it into an integer.
+    Arg: n (float)
+    Return: int
+    """
     return int(n)
