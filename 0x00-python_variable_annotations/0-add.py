25e3ca79ba63c5b68c3c79be124224dff5932d87
LionelMv <mvangicode@gmail.com>
2023-10-06 08:16:38 +0300

    doc: Add docstring on module and the function.
    Follow Google Style Python Docstrings

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""
+Adds two floats together.
+"""
+
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floats together.
+    Args:
+        a: float
+        b: float
+    Return a + b
+    """
     return a + b
