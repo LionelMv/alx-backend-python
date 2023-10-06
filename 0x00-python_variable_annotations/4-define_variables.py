8f2e3e804fd11d05c340f279dcee2c5a87ad74fc
LionelMv <mvangicode@gmail.com>
2023-10-06 08:29:51 +0300

    Add annotations to each variable.
    ALX rule even though works without annotations.

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -3,7 +3,7 @@
 Module shows how mypy infers types automatically by assignment.
 """
 
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
