1ed72818fa934b354e50cb586d710c6a2c8a6d2d
LionelMv <mvangicode@gmail.com>
2023-10-06 08:11:41 +0300

    doc: Add better docstring on function.
    Follow Google Style Python Docstrings.

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -7,9 +7,9 @@ This module takes two strings and returns the concatenated string
 def concat(str1: str, str2: str) -> str:
     """
     Concatenates two input strings and returns the result.
-
-    :param str1: The first string.
-    :param str2: The second string.
-    :return: The concatenated string.
+    Args:
+        str1: The first string.
+        str2: The second string.
+    Return: The concatenated string.
     """
     return str1 + str2
