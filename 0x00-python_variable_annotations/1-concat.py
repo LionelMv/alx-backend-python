2583457be421a4e6b391d3b889f8bdf4d6501ed0
LionelMv <mvangicode@gmail.com>
2023-10-06 07:51:32 +0300

    Change documentation from inside the function to outside

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""
+This module takes two strings and returns the concatenated string
+"""
+
 
 def concat(str1: str, str2: str) -> str:
-    """This function takes two strings and returns the concatenated string"""
     return str1 + str2
