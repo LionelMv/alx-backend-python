1e38f8a155e71943824dd428a6905ae494babd1e
LionelMv <mvangicode@gmail.com>
2023-10-10 12:57:57 +0300

    Add 4-tasks module and its test file

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous coroutine that spawns 'n' tasks of 'task_wait_random'
+    with the specified 'max_delay'.
+
+    Args:
+        n (int): The number of tasks to spawn.
+        max_delay (int): The maximum delay in seconds for each task.
+
+    Returns:
+        List[float]: A list of delays (float values) in ascending order.
+    """
+    delays = await asyncio.gather(*(task_wait_random(max_delay)
+                                    for _ in range(n)))
+    return sorted(delays)
