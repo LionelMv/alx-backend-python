6b06092f39e2dfccc419f285440a57e266b0ae92
LionelMv <mvangicode@gmail.com>
2023-10-10 11:53:23 +0300

    fix: Change measure_time to normal function from coroutine.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,21 +6,25 @@ import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
-    Measure the average execution time for running
-    the 'wait_n' coroutine 'n' times with the specified 'max_delay'.
+    Measure the total execution time for running the wait_n coroutine
+        'n' times with the specified 'max_delay'.
 
     Args:
-        n (int): The number of times to run the 'wait_n' coroutine.
-        max_delay (int): The maximum delay in seconds
-            for each call to 'wait_n'.
+        n (int): The number of times to run the wait_n coroutine.
+        max_delay (int): The maximum delay in seconds for each call to wait_n.
 
     Returns:
         float: The average execution time per iteration in seconds.
     """
-    s = time.time()
+    start_time = time.time()  # Record the start time
+
+    # Run the wait_n coroutine 'n' times with the specified 'max_delay'
     asyncio.run(wait_n(n, max_delay))
-    # await wait_n(n, max_delay)
-    total_time = time.time() - s
+
+    end_time = time.time()  # Record the end time
+    total_time = end_time - start_time
+
+    # Calculate and return the average time per coroutine execution
     return total_time / n
