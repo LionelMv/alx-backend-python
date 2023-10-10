4e070f9826796f870c38cdaae42773a7cb4867d1
LionelMv <mvangicode@gmail.com>
2023-10-10 12:26:49 +0300

    Add 3-tasks module and its test file

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function that takes an integer, returns a asyncio.Task"""
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    takes max_delay as an argument and return asyncio.Task
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
