2d3f84a311f9df23aef6a155ef8677b6ced26c85
LionelMv <mvangicode@gmail.com>
2023-10-11 09:59:26 +0300

    fix: Change AsyncGenerator to Generator annotation.
    ALX specification.
    Does not conform to mypy.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Yields a random float value between 0 and 10."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous coroutine that yields random numbers
         after a 1-second delay in each iteration.
